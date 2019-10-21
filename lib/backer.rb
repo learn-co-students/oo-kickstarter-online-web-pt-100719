8fae22fb1f9f2341c9369dedcf2c79e22ee7052b
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 01:58:35 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,22 @@
+class Backer
+  
+  attr_accessor :name :project
+  
+  def initialize(name)
+    @name = name
+    backer = Backer.new("Avi")
+  end
+  
+  def backer_project(project)
+    backer_project = []
+  
+end
+
+
+
+
+awesome_project = Project.new("This is an Awesome Project")
+ 
+bob.back_project(awesome_project)
+ 
+bob.backed_projects
